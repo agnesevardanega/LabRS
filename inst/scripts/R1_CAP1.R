68cb7c7266b320ab628a91c4e4f51b9f1cb21675
agnesevardanega <avardanega@gmail.com>
2018-08-21 18:15:25 +0200

    file aggiunto

---
+++ inst/scripts/R1_CAP1.R
@@ -0,0 +1,70 @@
+# Ricerca Sociale con R
+# Agnese Vardanega - avardanega@unite.it
+
+# CAPITOLO 1
+
+
+
+# LA CONSOLE DI R ---------------------------------------------------------
+
+3 + 2
+
+# FUNZIONI DI AIUTO
+help(rm)
+?rm
+??chisq
+
+# demo
+demo(lm.glm)
+
+
+# WORKSPACE ---------------------------------------------------------------
+
+# operatore di assegnamento
+a <- 3 + 5
+a
+
+# elenco degli oggetti
+ls()
+
+# rimuovere uno o più oggetti
+rm(a)
+
+# salvare e caricare area di lavoro
+# save.image("C:/Documenti/mydir/myfile.RData")
+save.image(file.choose())
+
+# caricare area di lavoro
+# load("C:/Documenti/mydir/myfile.RData")
+load(file.choose())
+
+
+# RCOMMANDER --------------------------------------------------------------
+
+install.packages("Rcmdr", dependencies = TRUE)
+library(Rcmdr)
+
+
+# DIRECTORY DI LAVORO -----------------------------------------------------
+
+# per sapere quale è:
+getwd()
+
+# per cambiarla:
+# setwd("C:/Documenti/mydir")
+setwd(choose.dir())
+
+
+# USARE GLI SCRIPTS DI LabRS -------------------------------------------------
+
+# LabRS
+install.packages("devtools", dependencies = TRUE)
+devtools::install_github("agnesevardanega/LabRS")
+
+# per trovare i file del pacchetto:
+system.file("extdata", package = "LabRS")
+system.file("scripts", package = "LabRS")
+system.file(package = "LabRS")
+
+# per aprire la cartella
+shell.exec("C:/.../Documents/R/win-library/3.5/LabRS/")
