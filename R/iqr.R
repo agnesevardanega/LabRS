010ef21fdb58fb90e1682b2b85f6af20644b1c35
agnesevardanega <avardanega@gmail.com>
2018-10-04 09:25:40 +0200

    modifiche e aggiute

---
+++ R/iqr.R
@@ -0,0 +1,24 @@
+#' Differenza interquartile (fattori ordinati)
+#'
+#' Calcola la differenza interquartile anche per i fattori ordinati.
+#' Per default, considera solo i casi validi.
+#'
+#' @param x vettore o fattore ordinato
+#' @param na.rm valore logico (TRUE o FALSE) (vedi \code{\link[stats]{IQR}})
+#' @param ... altri argomenti ereditati da \code{\link[stats]{IQR}}
+#' @return quantili (valori o livelli)
+#' @examples iqr(Orange$Tree) # fattore ordinato
+#' iqr(cars$speed) # vettore numerico
+#' @export
+iqr <- function (x, na.rm = TRUE, ...)
+{
+  if (is.factor(x)) {
+    if (!is.ordered(x)) {
+      warning("I quartili non si possono calcolare!!!")
+      return(NA)
+    }
+    IQR(unclass(x), na.rm, ...)
+  }
+  else
+    IQR(x, na.rm, ...)
+}
